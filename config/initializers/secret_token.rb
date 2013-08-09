f98fbaf7ceeab5eedb32cec666c786e349b8612f
ktlo <roggerbass@gmail.com>
2013-08-09 19:45:22 +0800

    first commit
    a dream to chase after

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whoru::Application.config.secret_key_base = '3d627718144930a3a81bccb6ec309981af0f92a424fd3a59c366e735d53b28fb85ac4dd51af27fe3961e9eefebef3d3f27a30da236e9624d85008239630ca953'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Whoru::Application.config.session_store :cookie_store, key: '_whoru_session'
